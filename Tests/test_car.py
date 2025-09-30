862d43f46f38ca881735ec6ad2404af9a915964e
ottom8cueeh-maker <ottom8cueeh@gmail.com>
2025-09-30 14:17:33 -0700

    revamp & reorganize:  part 2

--- Tests/test_car.py
+++ Tests/test_car.py
@@ -1,8 +1,14 @@
-def square(a):
-    return a * a
+def calculate_fuel_economy(litres_used, km_travelled):
+    return 100 / km_travelled * litres_used
 
-def test_square():
-    assert square(5) > 10
+def calculate_distance_by_fuel(fuel_left, fuel_econ):
+    return fuel_left/fuel_econ * 100        # distance can travel
 
-def test_always_true():
-    assert 2 == 2
+
+
+def test_enough_fuel_for_trip(tripkm=100):
+    assert calculate_distance_by_fuel(10, 5) >= tripkm
+
+def test_fuel_economy():
+    # A good fuel consumption rate is below (<) 6 litres per 100 kilometers
+    assert calculate_fuel_economy(4, 90) <= 6
