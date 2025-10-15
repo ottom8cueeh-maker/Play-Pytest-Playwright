da1aadc89cad39d94487fc7173f11bf5c2f1a069
ottom8cueeh-maker <ottom8cueeh@gmail.com>
2025-10-14 22:55:57 -0700

    test_car.py:  add dummy_func()

--- Tests/test_car.py
+++ Tests/test_car.py
@@ -5,10 +5,12 @@ def calculate_distance_by_fuel(fuel_left, fuel_econ):
     return fuel_left/fuel_econ * 100        # distance can travel
 
 
-
 def test_enough_fuel_for_trip(tripkm=100):
     assert calculate_distance_by_fuel(10, 5) >= tripkm
 
 def test_fuel_economy():
     # A good fuel consumption rate is below (<) 6 litres per 100 kilometers
     assert calculate_fuel_economy(4, 90) <= 6
+
+def dummy_func():
+    assert 'a' == 'a'
