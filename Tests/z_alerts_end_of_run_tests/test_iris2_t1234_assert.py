a51389b2fe587ebfc6cf94afcba23478078d37ec
ottom8cueeh-maker <ottom8cueeh@gmail.com>
2025-11-08 11:51:48 -0800

    commit on g_dev1

--- Tests/z_alerts_end_of_run_tests/test_iris2_t1234_assert.py
+++ Tests/z_alerts_end_of_run_tests/test_iris2_t1234_assert.py
@@ -1,11 +1,15 @@
 import datetime
+from datetime import timedelta
 
 def test_day_of_week():
     # Get today's date
     today = datetime.date.today()
 
+
+    anyday = today + timedelta(days=2)
+
     # Get the day of the week
-    assert today.strftime("%A")=="Friday"
+    assert anyday.strftime("%A")=="Monday"
 
 
 def test_is_leap_year(year=2024):
